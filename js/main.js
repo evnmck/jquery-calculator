3da8b0931b58aae52f09129af678da091b3a69d9
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-05-03 08:15:15 -0600

    works except for multipl expressions at once

--- js/main.js
+++ js/main.js
@@ -2,23 +2,26 @@ var numString = '',
     numArray = [],
     opArray = [],
     screenArray = [],
+    answerFlag = false,
     symbol,
-    numTally = 0,
-    showsAnswer = false
+    numTally = 0
 
 $(() => {
     $('span').on('click', function() {
         symbol = $(this).text()
-        if (showsAnswer === true) {
-            screenArray = []
-            $('#screen').html(screenArray)
-            showsAnswer = false
-        } else if (symbol.search(/[0-9]/g) != -1) { // check for number
+        if (symbol.search(/[0-9]/g) != -1) {
+            if (answerFlag === true) {
+                answerFlag = false
+                screenArray = []
+            }
             numString += symbol //add more numbers to string
             numTally++ //prove there was number input
             screenArray.push(' ', symbol, ' ')
-            $('#screen').html(screenArray)
         } else {
+            if (answerFlag === true) {
+                answerFlag = false
+                numTally++
+            }
             switch (symbol) {
                 case '+':
                 case '-':
@@ -36,52 +39,63 @@ $(() => {
                     } else {
                         operator(symbol)
                         screenArray = []
-                        tally = 0
                         showAnswer()
-                        $('#screen').html(screenArray)
-                        showsAnswer = true
                         break;
                     }
                 case 'C':
+                    numTally = 0
                     numString = ''
                     numArray = []
                     opArray = []
                     screenArray = []
-                    $('#screen').html(screenArray)
                     break;
             }
         }
+        $('#screen').html(screenArray)
+
+
+        function operator(symbol) {
+            numTally = 0
+            numArray.push(parseInt(numString))
+            opArray.push(symbol)
+            screenArray.push(' ', symbol, ' ')
+            numString = ''
+        }
+
+        function showAnswer() {
+            answerFlag = true
+            opArray.pop()
+            while (opArray.length > 0) {
+                var firstNum = numArray[numArray.length - 2]
+                var secondNum = numArray[numArray.length - 1]
+                var lastOp = opArray[opArray.length - 1]
+                var result = 0
+                if (lastOp === '/') {
+                    result = Math.round(firstNum / secondNum * 100) / 100
+                    numArray.pop()
+                    numArray.pop()
+                    opArray.pop()
+                } else if (lastOp === 'x') {
+                    result = firstNum * secondNum
+                    numArray.pop()
+                    numArray.pop()
+                    opArray.pop()
+                } else if (lastOp === '+') {
+                    result = firstNum + secondNum
+                    numArray.pop()
+                    numArray.pop()
+                    opArray.pop()
+                } else if (lastOp === '-') {
+                    result = firstNum - secondNum
+                    numArray.pop()
+                    numArray.pop()
+                    opArray.pop()
+                }
+                numArray.push(result)
+            }
+            result = Math.round(result * 100) / 100
+            screenArray.push(result)
+        }
+
     })
 })
-
-
-function operator(symbol) {
-    numTally = 0
-    numArray.push(parseInt(numString))
-    opArray.push(symbol)
-    screenArray.push(' ', symbol, ' ')
-    $('#screen').html(screenArray)
-    numString = ''
-
-}
-
-function showAnswer() {
-    var result = 0
-    var firstNum = numArray[numArray.length - 2]
-    var secondNum = numArray[numArray.length - 1]
-    opArray.pop()
-    if (opArray[opArray.length - 1] === '/') {
-        result = firstNum / secondNum
-        numArray.pop()
-        numArray.pop()
-        opArray.pop()
-    } else if (opArray[opArray.length - 1] === 'x') {
-        result = firstNum * secondNum
-        numArray.pop()
-        numArray.pop()
-        opArray.pop()
-    }
-
-    screenArray.push(Math.round(result * 100) / 100)
-
-}
